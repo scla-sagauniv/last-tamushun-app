6fcc795c284f626bafc4eca15df2fca7671e776a
shunsuke-tamura <wwwxx4869@gmail.com>
2024-03-20 13:04:18 +0900

    bugfix: firebaseInitでオプションを読み込むようにした

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,13 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:last_tamushun_app/firebase_options.dart';
 import 'package:last_tamushun_app/router.dart';
 
 void main() async {
   // 初期化処理を追加
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   runApp(const ProviderScope(child: MaterialApp(home: MyApp())));
 }
 
